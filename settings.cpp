e652c16415387ed9a1804065d34c3ac25ca73a68
Frits <sendie61@hotmail.com>
2022-07-23 23:05:31 +0200

    implemented

--- settings.cpp
+++ settings.cpp
@@ -1,6 +1,34 @@
 #include "settings.h"
+#include <QSettings>
 
 Settings::Settings()
 {
-
+    // initialze machine settings
+    GetMachineSettings();
+    // initialze stock settings
+    GetStockSettings();
 }
+
+void Settings::GetMachineSettings()
+{
+    QSettings persistantSettings(ORGANIZATION,APPNAME);
+
+    persistantSettings.beginGroup("Machine");
+    Machine.constandSurfaceSpeed= persistantSettings.value("constandSurfaceSpeed",true).toBool();
+    Machine.maxSpindleSpeed= persistantSettings.value("maxSpindleSpeed",MAXSPINDLESPEED).toDouble();
+    Machine.maxZfeed= persistantSettings.value("maxZfeed",MAXZFEED).toDouble();
+    Machine.maxXfeed= persistantSettings.value("maxXfeed",MAXXFEED).toDouble();
+    persistantSettings.endGroup();
+}
+
+void Settings::GetStockSettings()
+{
+    QSettings persistantSettings(ORGANIZATION,APPNAME);
+
+    persistantSettings.beginGroup("Stock");
+    Stock.diameter= persistantSettings.value("diameter",20).toDouble();
+    Stock.stickOut= persistantSettings.value("stickOut",100).toDouble();
+    Stock.material= persistantSettings.value("material",3).toInt();
+    persistantSettings.endGroup();
+}
+
--- settings.h
+++ settings.h
@@ -1,12 +1,51 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-#include <QObject>
+#define ORGANIZATION    "TechSkill"
+#define APPNAME         "TS_Turning"
+
+#define MAXXFEED        3000
+#define MAXZFEED        4000
+#define MAXSPINDLESPEED 3000
+
+
+#include <QStringList>
 
 class Settings
 {
 public:
     Settings();
+    void GetMachineSettings();
+    void GetStockSettings();
+
+    struct Stock
+    {  // stock
+        const QStringList materials =
+        {                           // material
+            "Aluminum",             // 0
+            "High carbon steel",    // 1
+            "Mid. carbon steel",    // 2
+            "Low carbon steel",     // 3
+            "Stainless steel",      // 4
+            "Brass",                // 5
+            "Bronze",               // 6
+            "Copper",               // 7
+            "Titanium",             // 8
+            "Plastics"              // 9
+        };
+        int material= 3;            // default Low carbon steel
+        double stickOut= 100;       // 100mm stickout
+        double diameter= 20;        // 20mm diameter
+    } Stock;
+
+    struct Machine
+    {  //machine
+        double maxZfeed= 4000;
+        double maxXfeed= 3000;
+        double maxSpindleSpeed= 3000;
+        bool constandSurfaceSpeed;
+    } Machine;
+
 };
 
 #endif // SETTINGS_H
