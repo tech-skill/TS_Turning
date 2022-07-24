fd8d54fb694fc46b8762b34507908fbb5a35daee
Frits <sendie61@hotmail.com>
2022-07-24 11:32:30 +0200

    tests for Stock

--- test/test_settings.cpp
+++ test/test_settings.cpp
@@ -10,3 +10,18 @@ TEST_CASE("default Machine Settings", "[fancy]")
     REQUIRE(dut.Machine.maxZfeed == MAXZFEED);
     REQUIRE(dut.Machine.constandSurfaceSpeed == true);
 }
+
+TEST_CASE("default Stock Settings", "[fancy]")
+{
+    Settings dut;
+    REQUIRE(dut.Stock.diameter == 20);
+    REQUIRE(dut.Stock.material == 3);
+    REQUIRE(dut.Stock.stickOut == 100);
+}
+
+TEST_CASE("Materials Settings", "[fancy]")
+{
+    Settings dut;
+    REQUIRE(dut.Stock.materials.count() == 10);     // there should be 10 different materials
+
+}
